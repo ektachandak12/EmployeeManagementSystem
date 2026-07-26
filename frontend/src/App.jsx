06890975b4b493d3220c9bd06a95e61e6e08e74d
ektachandak12 <ektachandak.edu@gmail.com>
2026-07-26 23:55:14 +0530

    Configured rounting in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+ 
 
 function App() {
   return (
     <>
-        <HeaderComponent />
-        <ListEmployeeComponent />
-        <FooterComponent />
+        <BrowserRouter>
+            <HeaderComponent />
+            <Routes>
+                <Route path="/" element={<ListEmployeeComponent />} />
+                <Route path="/employees" element={<ListEmployeeComponent />} />
+            </Routes>
+            <FooterComponent />
+        </BrowserRouter>
     </>
   );
 }
