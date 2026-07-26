4b765110a745d63bd3de68e93c1d23e922bc6238
ektachandak12 <ektachandak.edu@gmail.com>
2026-07-26 23:34:39 +0530

    Added Header and Footer COmponent

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span>
+                All Rights Reserved 2026 by Ekta Chandak
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
